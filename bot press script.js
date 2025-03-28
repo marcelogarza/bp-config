02aff62d1514f5b06632e23a80159f5247212a6d
Lil crypto <32376100+marcelogarza@users.noreply.github.com>
2025-03-28 11:32:36 -0400

    Add files via upload

---
+++ bot press script.js
@@ -0,0 +1,37 @@
+window.botpress.init({
+  "botId": "a466b2ec-c274-464c-bdda-e3a0fd477235",
+  "configuration": {
+    "composerPlaceholder": "",
+    "botName": "Moneyfi Chat",
+    "botAvatar": "https://files.bpcontent.cloud/2024/09/21/21/20240921215510-IQ8NPT52.png",
+    "botDescription": "Ask me anything 🤑\n",
+    "website": {
+      "title": "",
+      "link": ""
+    },
+    "email": {
+      "title": "",
+      "link": ""
+    },
+    "phone": {
+      "title": "",
+      "link": ""
+    },
+    "termsOfService": {
+      "title": "",
+      "link": ""
+    },
+    "privacyPolicy": {
+      "title": "",
+      "link": ""
+    },
+    "color": "#3e63dd",
+    "variant": "solid",
+    "themeMode": "light",
+    "fontFamily": "rubik",
+    "radius": 2,
+    "showPoweredBy": false,
+    "allowFileUpload": true
+  },
+  "clientId": "dd3bad18-ccb1-4995-97c1-0bdb894316cb"
+});
